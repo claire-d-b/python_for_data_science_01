dac53bf980d3c727e45972084a99b63c2f31f34c
claire-d-b <claire.deberny@essca.eu>
2024-07-04 12:25:02 +0200

    Calculate items in pixel and clean code

--- ex02/load_image.py
+++ ex02/load_image.py
@@ -1,23 +1,26 @@
 from PIL import Image
 
 def ft_load(path: str) -> bytearray:
-    image = Image.open(path)
-    barray = []
-
-    width, height = image.size
-
-    for x in range(width):
-        for y in range(height):
-            r, g, b = image.getpixel((x, y))
-            barray.append([r, g, b])
-    
-    barray.append([r, g, b])
-    string = "The shape of image is: "
     try:
-        assert len(barray) % 3 == 0, "Error: file is not in correct format"
-        print(string, (height, width, 3))
+        assert Image.open(path), "Error: failed to open file"
+        image = Image.open(path)
+        barray = []
+
+        width, height = image.size
+
+        for x in range(width):
+            for y in range(height):
+                r, g, b = image.getpixel((x, y))
+                barray.append([r, g, b])
+        barray.append([r, g, b])
+        
+        string = "The shape of image is: "
+        items = image.getpixel((0,0))
+        i = 0
+        for item in items:
+            i += 1
+        print(string, height, width, i)
     except AssertionError as e:
         print(f"AssertionError: {e}")
-        return None
     return barray
 
